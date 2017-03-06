00eb8ab4822d52993eb490f444974a244dad7853
Ed de Almeida <edvaldoajunior@gmail.com>
2017-03-06 17:53:16 -0300

    Basic specs passing

--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -2,10 +2,32 @@ require "spec_helper"
 
 RSpec.describe EmeraldFW::CLI do
 
-  context 'EmeraldFW::CLI configuration' do
-  
-    
-  
+  context 'Errors' do
+
+  	it 'EmeraldFW::CLI raises an error when first parameter is not an array' do
+  	  expect {
+  	  	EmeraldFW::CLI.new('potato',{})
+  	  }.to raise_error(ArgumentError,"First parameter is meant to be an array")
+  	end
+
+  	it 'EmeraldFW::CLI raises an error when receives an invalid entity' do
+  	  expect {
+  	  	EmeraldFW::CLI.new(['potato','chips'],{})
+  	  }.to raise_error(ArgumentError,"Invalid entity: potato")
+  	end
+
+  	it 'EmeraldFW::CLI raises an error when receives an invalid command' do
+  	  expect {
+  	  	EmeraldFW::CLI.new(['project','chips'],{})
+  	  }.to raise_error(ArgumentError,"Invalid command: chips")
+  	end
+
+  	it 'EmeraldFW::CLI raises an error when second parameter is not a hash' do
+  	  expect {
+  	  	EmeraldFW::CLI.new(['project','create'],'teste')
+  	  }.to raise_error(ArgumentError,"Second parameter is meant to be a hash")
+  	end
+
   end
 
 end

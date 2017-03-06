85a86256f40140c5a20011016c4c28700d3a354e
Ed de Almeida <edvaldoajunior@gmail.com>
2017-03-06 03:40:10 -0300

    Refactoring

--- lib/emeraldfw.rb
+++ lib/emeraldfw.rb
@@ -1,16 +1,9 @@
 require "emeraldfw/cli"
 require "emeraldfw/version"
 
-require "entities/component"
-require "entities/component/generator"
-require "entities/component/remover"
-require "entities/page"
-require "entities/page/generator"
-require "entities/page/remover"
-
 module EmeraldFW
 
-  VALID_ENTITIES = [:component, :library, :project, :resource]
+  VALID_ENTITIES = [:component, :library, :project, :resource].freeze
 
   VALID_COMMANDS = [:create, :remove]
 
